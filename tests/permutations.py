dbe7800f2599b7ef8a35376ce46b54a33d3a7729
Ian Atkin <ian@versal.com>
2021-06-03 14:57:38 -0700

    initial commit

---
+++ tests/permutations.py
@@ -0,0 +1,41 @@
+from itertools import permutations, product
+
+def p1(iterable, r=None):
+    # permutations('ABCD', 2) --> AB AC AD BA BC BD CA CB CD DA DB DC
+    # permutations(range(3)) --> 012 021 102 120 201 210
+    pool = tuple(iterable)
+    n = len(pool)
+    r = n if r is None else r
+    if r > n:
+        return
+    indices = list(range(n))
+    print("indices: %s" % (indices))
+    cycles = list(range(n, n-r, -1))
+    print("cycles: %s" % (cycles))
+    yield tuple(pool[i] for i in indices[:r])
+    print("pool: %s" % (str(pool)))
+
+    while n:
+        for i in reversed(range(r)):
+            cycles[i] -= 1
+            if cycles[i] == 0:
+                indices[i:] = indices[i+1:] + indices[i:i+1]
+                cycles[i] = n - i
+            else:
+                j = cycles[i]
+                indices[i], indices[-j] = indices[-j], indices[i]
+                yield tuple(pool[i] for i in indices[:r])
+                break
+        else:
+            return
+
+def p2(iterable, r=None):
+    pool = tuple(iterable)
+    n = len(pool)
+    r = n if r is None else r
+    for indices in product(range(n), repeat=r):
+        if len(set(indices)) == r:
+            yield tuple(pool[i] for i in indices)
+
+perms = [''.join(p) for p in p1('atkin')]
+print(perms)
